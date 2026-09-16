b3d886ff95d959b3cb423fd51ca9c2707f69bb4a
Yaswanth-rajana <yaswanthrajanaindiann@gmail.com>
2026-09-16 22:00:41 +0530

    Fix module resolution for external backend directory

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -11,6 +13,10 @@ const nextConfig = {
         hostname: 'images.unsplash.com',
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.modules.push(path.resolve(__dirname, 'node_modules'));
+    return config;
   },
 };
 
